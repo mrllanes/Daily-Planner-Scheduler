bfc01fba47e022d77b5d43cde1107fc8efeeb655
Mr. Llanes <llanesm@gmail.com>
2021-01-03 12:45:55 -0600

    Changes to all 3 files.

--- Assets/script.js
+++ Assets/script.js
@@ -1,30 +1,17 @@
 // A $( document ).ready() block.
 $(document).ready(function() {
-    console.log("ready!");
-
-    var utc = require('dayjs/plugin/utc') // dependent on utc plugin
-    var timezone = require('dayjs/plugin/timezone')
-    dayjs.extend(utc)
-    dayjs.extend(timezone)
-    
-    dayjs.tz("2014-06-01 12:00", "America/New_York")
-    
-    dayjs("2014-06-01 12:00").tz("America/New_York")
-    
-    dayjs.tz.guess()
-    
-    dayjs.tz.setDefault("America/New_York")
+    console.log("document ready!");
 
 
-$("#currentDay").text(dayjs.tz.guess());
+    // Date and time display
+    $("#currentDay").text(dayjs().format('MM-DD-YYYY HH:mm:ss'));
 
-
+// Save Button code to store information entered
 $(".saveBtn").on("click", function () {
     var description = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
     localStorage.setItem(time, description);
-
 });
 
 
