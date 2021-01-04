b4bc551d54d9702187c6eb8df8d2d585f521229b
Mr. Llanes <llanesm@gmail.com>
2021-01-04 00:40:38 -0600

    JS file updated, local Storage displayed on screen.

--- Assets/script.js
+++ Assets/script.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
     console.log("document ready!");
 
 
-    // Date and time display
-    $("#currentDay").text(dayjs().format('MM-DD-YYYY HH:mm:ss'));
+// Date and time display
+$("#currentDay").text(dayjs().format('MM-DD-YYYY HH:mm:ss'));
 
 // Save Button code to store information entered
 $(".saveBtn").on("click", function () {
@@ -15,10 +15,37 @@ $(".saveBtn").on("click", function () {
 });
 
 
+function updatedHour () {
+    var currentHour = dayjs().format('H');
+    console.log(currentHour);
+    $('.time-block').each(function() {
+        var blockHour = parseInt($(this).attr("id"));
+        console.log(blockHour);
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+        } else if (blockHour == currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
 
+updatedHour ();
 
-
-
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
 
 });
